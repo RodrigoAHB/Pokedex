5d35f0092fa85bf4d2d2eb492a4294a5a4d41722
Rodrigo Albano Hofer Bastos <digoalbano1010@gmail.com>
2024-04-19 00:22:18 -0300

    Style: finishing stilization and first version of design

--- Pokedex/src/App.vue
+++ Pokedex/src/App.vue
@@ -3,7 +3,6 @@
   import axios from 'axios';
 
   const url = "https://pokeapi.co/api/v2/pokemon/pikachu"
-  const count = ref(0)
 
   axios.get(url)
   .then(response => {
@@ -15,8 +14,20 @@
   <v-app>
     <v-main class="app">
       <v-container>
-        <header class="d-flex flex-column align-center flex-sm-row justify-sm-space-between align-sm-end">
-          <img src="/img/pokemon-23.svg" alt="" class="pokemonLogo cursor-pointer mb-4">
+        <header >
+          <v-form class="d-flex flex-row justify-space-between">
+            <v-row class="align-end">
+              <v-col cols="12" sm="4" md="3" lg="2" class="d-flex">
+                <v-img src="/img/pokemon-23.svg" class="pokemonLogo cursor-pointer mb-1 mb-sm-4"></v-img>
+              </v-col>
+              <v-col cols="5" sm="3" md="2" lg="2">
+                <v-select variant="solo-filled" density="compact" class="form__select" :items="['Nome', 'Id', 'Tipo', 'Espécie']" placeholder="Filtro" ></v-select>
+              </v-col>
+              <v-col cols="7" sm="5" md="7" lg="8" class="d-flex" >
+                <v-text-field class="form__text-field" variant="solo-filled" density="compact" append-inner-icon="mdi-magnify"></v-text-field>
+              </v-col>
+            </v-row>  
+          </v-form>
         </header>
         <v-row>
           <v-col cols="12" sm="6" md="4" lg="3" xl="2">
@@ -26,7 +37,7 @@
                 <v-card-title>Pikachu</v-card-title>
               </v-img>
               <v-card-subtitle class="mb-1 card_subtitle-color">
-                #0025 {{ count }}
+                #0025
               </v-card-subtitle>
             </v-card>
           </v-col>
@@ -37,7 +48,7 @@
                 <v-card-title>Pikachu</v-card-title>
               </v-img>
               <v-card-subtitle class="mb-1 card_subtitle-color">
-                #0025 {{ count }}
+                #0025
               </v-card-subtitle>
             </v-card>
           </v-col>
@@ -48,7 +59,7 @@
                 <v-card-title>Pikachu</v-card-title>
               </v-img>
               <v-card-subtitle class="mb-1 card_subtitle-color">
-                #0025 {{ count }}
+                #0025
               </v-card-subtitle>
             </v-card>
           </v-col>
@@ -59,7 +70,7 @@
                 <v-card-title>Pikachu</v-card-title>
               </v-img>
               <v-card-subtitle class="mb-1 card_subtitle-color">
-                #0025 {{ count }}
+                #0025
               </v-card-subtitle>
             </v-card>
           </v-col>
@@ -75,6 +86,10 @@
   }
   .pokemonLogo{
     width: 175px;
+    height: 70px;
+  }
+  .form__text-field .v-field__append-inner:hover{
+    cursor: pointer;
   }
   .card{
     background: white;
@@ -85,7 +100,6 @@
   .card_subtitle-color{
     color: lightslategray;
   }
-
   @media screen and (max-width: 959px){
     .v-container{
       max-width: 620px;

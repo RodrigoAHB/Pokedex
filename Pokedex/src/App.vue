3242de83836260413a78fec49a4c20066dab3d19
Rodrigo Albano Hofer Bastos <digoalbano1010@gmail.com>
2024-04-21 12:55:40 -0300

    feat: adding infinite scroll and conection to the API

--- Pokedex/src/App.vue
+++ Pokedex/src/App.vue
@@ -1,13 +1,56 @@
 <script setup>
-  import { ref } from 'vue';
+  import { reactive, ref , toRaw, onMounted} from 'vue';
   import axios from 'axios';
 
-  const url = "https://pokeapi.co/api/v2/pokemon/pikachu"
+  import { useInfiniteScroll } from '@vueuse/core'
 
-  axios.get(url)
-  .then(response => {
-    console.log(response.data)
+  const baseUrl = ref('https://pokeapi.co/api/v2/')
+  const pokemonsEndPoint = ref('pokemon?limit=10')
+  const pokemons = ref([])
+  const listEl = ref(null)
+  const pokemonsToShow = 10
+
+
+  const getPokemons = async (limit, skip) => {
+    const response = await axios.get(
+    `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${skip}`
+    );
+    return response.data.results;
+  }
+
+  function getPokemonId(pokemon){
+    return pokemon.url.split("/")[6]
+  }
+
+  onMounted(() => {
+    getPokemons(pokemonsToShow, 0)
   })
+
+  // Change firs letter of pokemon name do upper case
+  function toUpper(name){
+    return name.replace(/^\w/, (firstLetter) => firstLetter.toUpperCase());
+  }
+
+
+  function showPokemons(){
+    console.log(pokemons.value)
+  }
+
+  const getPokemonsOnScroll = async () => {
+    const newPokemons =  await getPokemons(
+      pokemonsToShow,
+      pokemons.value.length
+    )
+    pokemons.value.push(...newPokemons)
+  } 
+
+  useInfiniteScroll(
+    listEl,
+    async () => {
+     await getPokemonsOnScroll()
+    },
+    {distance: 10}
+  )
 </script>
 
 <template>
@@ -21,60 +64,29 @@
                 <v-img src="/img/pokemon-23.svg" class="pokemonLogo cursor-pointer mb-1 mb-sm-4"></v-img>
               </v-col>
               <v-col cols="5" sm="3" md="2" lg="2">
-                <v-select variant="solo-filled" density="compact" class="form__select" :items="['Nome', 'Id', 'Tipo', 'Espécie']" placeholder="Filtro" ></v-select>
+                <v-select variant="solo-filled" density="compact" :items="['Nome', 'Id', 'Tipo', 'Espécie']" v-model="filterSelected" placeholder="Filtro"></v-select>
               </v-col>
               <v-col cols="7" sm="5" md="7" lg="8" class="d-flex" >
-                <v-text-field class="form__text-field" variant="solo-filled" density="compact" append-inner-icon="mdi-magnify"></v-text-field>
+                <v-text-field class="form__text-field" variant="solo-filled" density="compact" append-inner-icon="mdi-magnify" v-model="searchInput" @click:append-inner="showPokemons" ></v-text-field>
               </v-col>
             </v-row>  
           </v-form>
         </header>
-        <v-row>
-          <v-col cols="12" sm="6" md="4" lg="3" xl="2">
-            <v-card variant="flat" class="card border">
-              <v-img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
-              class="align-end text-black card__img">
-                <v-card-title>Pikachu</v-card-title>
-              </v-img>
-              <v-card-subtitle class="mb-1 card_subtitle-color">
-                #0025
-              </v-card-subtitle>
-            </v-card>
-          </v-col>
-          <v-col cols="12" sm="6" md="4" lg="3" xl="2">
-            <v-card variant="flat" class="card">
-              <v-img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
-              class="align-end text-black card__img">
-                <v-card-title>Pikachu</v-card-title>
-              </v-img>
-              <v-card-subtitle class="mb-1 card_subtitle-color">
-                #0025
-              </v-card-subtitle>
-            </v-card>
-          </v-col>
-          <v-col cols="12" sm="6" md="4" lg="3" xl="2">
-            <v-card variant="flat" class="card">
-              <v-img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
-              class="align-end text-black card__img">
-                <v-card-title>Pikachu</v-card-title>
-              </v-img>
-              <v-card-subtitle class="mb-1 card_subtitle-color">
-                #0025
-              </v-card-subtitle>
-            </v-card>
-          </v-col>
-          <v-col cols="12" sm="6" md="4" lg="3" xl="2">
-            <v-card variant="flat" class="card">
-              <v-img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
-              class="align-end text-black card__img">
-                <v-card-title>Pikachu</v-card-title>
-              </v-img>
-              <v-card-subtitle class="mb-1 card_subtitle-color">
-                #0025
-              </v-card-subtitle>
-            </v-card>
-          </v-col>
-        </v-row>
+        <div ref="listEl" style="height: 100vh; overflow-y: auto;">
+          <v-row>
+            <v-col cols="12" sm="6" md="4" lg="3" xl="2" v-for="pokemon in pokemons" >
+              <v-card variant="flat" class="card border">
+                <v-img :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${getPokemonId(pokemon)}.png`" :alt="pokemon.name"
+                class="align-end text-black card__img">
+                  <v-card-title>{{ toUpper(pokemon.name) }}</v-card-title>
+                </v-img>
+                <v-card-subtitle class="mb-1 card_subtitle-color">
+                  {{ getPokemonId(pokemon).padStart(4, '0') }}
+                </v-card-subtitle>
+              </v-card>
+            </v-col>
+          </v-row>
+        </div>
       </v-container>
     </v-main>
   </v-app>
